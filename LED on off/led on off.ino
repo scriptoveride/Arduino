7274aea2d5e9ffbfffc05695061624a38bb7a442
Kai <83906674+scriptoveride@users.noreply.github.com>
2021-09-02 20:05:37 +1000

    Add files via upload

---
+++ LED on off/led on off.ino
@@ -0,0 +1,15 @@
+
+int LED = 13; // conect Led to arduino pin 5
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
