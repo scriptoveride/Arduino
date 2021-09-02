56172536ec1e2ae601a07952aaf7f75502c83323
Kai <83906674+scriptoveride@users.noreply.github.com>
2021-09-02 17:11:28 +1000

    Add files via upload

---
+++ LED on off with IR/LED on off with IR.ino
@@ -0,0 +1,23 @@
+
+int LED = 5; // conect Led to arduino pin 5
+
+int IRSensor = 2; // connect ir sensor to arduino pin 2
+
+
+void setup() 
+{
+  pinMode (IRSensor, INPUT); // sensor pin INPUT
+  pinMode (LED, OUTPUT); // Led pin OUTPUT
+}
+
+void loop() {
+  int statusSensor = digitalRead (IRSensor);
+  
+  if (statusSensor == 1) {
+    digitalWrite(LED, HIGH); // LED LOW
+  }
+  
+  else {
+    digitalWrite(LED, LOW); // LED High
+  }
+}
